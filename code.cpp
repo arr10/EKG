e1b5d13db7e12b66cbf0d558330923918916ab04
arr10 <72565007+arr10@users.noreply.github.com>
2020-10-25 15:06:36 +0600

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -6,21 +6,29 @@
 #include <algorithm>
 using namespace std;
 main(){
-	int n = 30;
-	int m = 70;
-	cout << "enter amount of sequences, and number of members of each sequence" << endl;
-	cin>> n >> m;
-	n+= 2;
-	m++;
+	int n = 2;
+	int m = 500;
+	int c1, c2;
 	int a[m + 1][n + 2];
 	int b[m + 1];
 	int c[m + 1];
 	int cnt = 2;
-	int answer[n][n];
+	int answer;
 	bool inList = 0;
-	for(int i = 2; i < n; i++){
-		a[1][i] = i;
-		cout << 1 << " " << i << " ";
+	bool same = 1;
+	
+	while(1){
+		cout << "input 2 numbers i and j, 0 0 to return" << endl;
+	cin >> c1 >> c2;
+	int temp[2];
+	temp[0] = c1;
+	temp[1] = c2;
+	if(c1 == 0) return 0;
+	
+	
+	for(int i = 2; i < 4; i++){
+		a[1][i] = temp[i-2];
+		cout << 1 << " " << temp[i-2] << " ";
 		for(int j = 2; j < m; j++){
 			while(1){
 				if(__gcd(cnt, a[j-1][i]) > 1){
@@ -32,7 +40,8 @@ main(){
 					}
 					if(!inList){
 						a[j][i] = cnt;
-						cout << cnt << " ";
+						if(j <= 70) cout << cnt << " ";
+						
 						break;
 					}
 					inList = 0;
@@ -43,19 +52,13 @@ main(){
 		}
 		cout<<"\n";
 	}
-	bool same = 1;
-	int c1, c2;
-	while(1){
-		cout << "input 2 numbers in descending order, 0 0 to return" << endl;
-	cin >> c1 >> c2;
-	if(c1 == 0) return 0;
-	for(int i1 = 3; i1 < n; i1++){
-		for(int i2 = 2; i2 < i1; i2++){
+	
+	
 			for(int j = m - 1; j >= 1; j--){
-				if(a[j][i1] != a[j][i2]){
+				if(a[j][2] != a[j][3]){
 					for(int k = j; k > 0; k--){
-						b[k] = a[k][i1];
-						c[k] = a[k][i2];
+						b[k] = a[k][2];
+						c[k] = a[k][3];
 					}
 					sort(b + 1, b + j + 1);
 					sort(c + 1, c + j + 1);
@@ -67,23 +70,22 @@ main(){
 					}
 					if(same){
 						//cout << i1 << " and " << i2 << " are same from " << j + 1 << endl;
-						answer[i1][i2] = j + 1;
+						answer = j + 1;
 					}
 					else{
 						//cout << i1 << " and " << i2 << " are not same in first " << m << " parts" << endl;
-						answer[i1][i2] = 0;
+						answer = 0;
 					}
 					same = 1;
 					break;
 				}
 			}
-		}
-	}
-	if(answer[c1][c2]){
-		cout << c1 << " and " << c2 << " are same from " << answer[c1][c2] << endl;
+
+	if(answer){
+		cout << "a" << c1 << " and " << "a" << c2 << " converge at " << answer << "   (only first 70 terms are shown above)" <<endl;
 	}
 	else{
-		cout << c1 << " and " << c2 << " are not same in first " << m << " parts" << endl;
+		cout << "a" << c1 << " and " << "a" << c2 << " converge at x > " << m << endl;
 	}
 	}
 	
